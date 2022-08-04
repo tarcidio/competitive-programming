936c8b830708008dc31833eef5e6c7fa2dc71766
tarcidio <tarcidio.antonio@usp.br>
2022-08-04 19:03:14 -0300

    Exercise C of the class4-stl-2

--- exercises/codeforces/gema/class4-stl-2/C.cpp
+++ exercises/codeforces/gema/class4-stl-2/C.cpp
@@ -1,5 +1,5 @@
 //NAME: T-shirt buying
-//
+//O(logn) ou O(n)
 
 #include <bits/stdc++.h>
 
@@ -12,35 +12,56 @@ using namespace std;
 //j: buyer number j
 //c_j: favorite color
 
-struct t_shirt{
-    int p;//price
-    int a;//front color
-    int b;//back color
-};
-
-const int MAX_N_M = 200001;
-
-vector<t_shirt> inventory (MAX_N_M);
-int buyers [MAX_N_M];
+const int MAXN = 2e6 + 1;
+int a[MAXN];//front color
+int b[MAXN];//back color
+int p[MAXN];//price
+int c[MAXN];//favorite color
+set<int> price_color[4];
+//price_color[0]: isn't used
+//price_color[1]: price of the t-shirts of color type 1
+//price_color[2]: price of the t-shirts of color type 2
+//price_color[3]: price of the t-shirts of color type 3
 
 int main (){
     int n;//numbers of shirts
     int m;//number os buyers
 
+    //read the tshirts
     cin >> n;
     for(int i = 0; i < n; i++)
-        cin >> inventory[i].p;
+        cin >> p[i];
     for(int i = 0; i < n; i++)
-        cin >> inventory[i].a;
+        cin >> a[i];
     for(int i = 0; i < n; i++)
-        cin >> inventory[i].b;
+        cin >> b[i];
+
+    //fill the set
+    for(int i = 0; i < n; i++){
+        price_color[a[i]].insert(p[i]);
+        price_color[b[i]].insert(p[i]);
+        //ex: if i = 0 take a[0]= 1 and p[0] = 500, then 
+        //price_color[1] = {500} 
+        //price_color[2] = {} 
+        //price_color[3] = {} 
+    }
+
+    //read the buyers
     cin >> m;
     for(int j = 0; j < m; j++)
-        cin >> buyers[j];
-
-    //https://www.cnblogs.com/EDGsheryl/p/6865151.html
-    //https://blog.csdn.net/tigerisland45/article/details/116355669
-    //https://blog.karatos.in/a?ID=00700-bef04cb4-2185-4711-a6e4-7b64a39f9efa
+        cin >> c[j];
+    
+    //algorithm
+    for(int j = 0; j < m; j++){
+        if(price_color[c[j]].empty()){
+            cout << "-1 ";
+        }else{
+            int price_of_the_color = *(price_color[c[j]].begin());//take the smallest element of the set
+            cout << price_of_the_color << " ";
+            for(int i = 1; i <= 3; i++)
+                price_color[i].erase(price_of_the_color);//this only is possible because p_i's are distinct
+        }
+    }
     
     /*
     This is VERY ineficient
---
+++ exercises/codeforces/gema/class4-stl-2/D.cpp
@@ -0,0 +1,11 @@
+//NAME: Yuhao and a Parenthesis
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+    return 0;
+}
