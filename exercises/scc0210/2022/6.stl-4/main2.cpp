7e6b1092c9f7055c4c8ce283fa09b86cd78fdacf
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2023-03-22 11:25:53 -0300

    Update main2.cpp
    This correct now

--- exercises/scc0210/2022/6.stl-4/main2.cpp
+++ exercises/scc0210/2022/6.stl-4/main2.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+//Explanation: she wants the biggest streak
 const int MAX = 1e9 + 1;
-int snowflakes_id[MAX];//what is your name?
+int snowflakes_id[MAX];
 
-int main(){
-    int num_cases, i, j, num_snowflakes, count, ans;
-    cin >> num_cases;
-    while(num_cases--){
-        cin >> num_snowflakes;
-        map<int,int> ma;//what is your name?
-        count = ans = 0;
-        for(int i = 0; i < num_snowflakes; i++)
-            cin >> snowflakes_id[i];
-        for(int j = 1; j <= num_snowflakes; j++){
-            
-        }
+int main() {
+  int num_cases, num_snowflakes, count, ans;
+  cin >> num_cases;
+  while (num_cases--) {
+    cin >> num_snowflakes;
+    map<int, int> ocurr_snowflake; // what is your name?
+    count = ans = 0;
+    for (int i = 0; i < num_snowflakes; i++)
+      cin >> snowflakes_id[i];
+    for (int j = 0; j < num_snowflakes; j++) {
+      if (ocurr_snowflake.find(snowflakes_id[j]) ==
+          ocurr_snowflake.end()) { // If
+        ocurr_snowflake[snowflakes_id[j]] = j;
+        count++;
+      } else {
+        if (count > ans)
+          ans = count;
+        j = ocurr_snowflake[snowflakes_id[j]];
+        ocurr_snowflake.clear();
+        count = 0;
+      }
     }
+    if (count > ans)
+      ans = count;
+    cout << ans << endl;
+  }
 
-    return 0;
+  return 0;
 }
