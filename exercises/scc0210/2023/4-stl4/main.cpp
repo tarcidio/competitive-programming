958486ffe05b38c285615a2c523ff2384a1ec1da
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2023-03-24 14:17:39 -0300

    Create main.cpp

---
+++ exercises/scc0210/2023/4-stl4/main.cpp
@@ -0,0 +1,51 @@
+// O(K*logN)
+// If we use the pq time[3000], we have O(N^3*logN)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef tuple<int, int, int> tiii;
+
+// cmp true: r1 < r2
+bool cmp(tiii r1, tiii r2) {
+  if (get<2>(r1) != get<2>(r2))
+    return get<2>(r1) > get<2>(r2);
+  else
+    return get<0>(r1) > get<0>(r2);
+}
+
+int main() {
+  string input;
+  cin >> input;
+  priority_queue<tiii, vector<tiii>, decltype(&cmp)> pq(&cmp);
+  while (input[0] != '#') {
+    tiii reg;
+    // First: id
+    // Second: period
+    // Third: time
+    cin >> get<0>(reg) >> get<1>(reg);
+    get<2>(reg) = get<1>(reg);
+    pq.push(reg);
+    cin >> input;
+  }
+
+  int K;
+  cin >> K;
+
+  bool flag = false;
+  if (K > 20)
+    flag = true;
+
+  while (K--) {
+    auto reg = pq.top();
+    pq.pop();
+    get<2>(reg) += get<1>(reg);
+    cout << get<0>(reg) << endl;
+    pq.push(reg);
+  }
+
+  if (flag)
+    cout << endl;
+
+  return 0;
+}
