9d606f2a114f4d66a5fa95bc35ca3442c4f95289
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2023-04-02 16:50:13 -0300

    Update main.cpp

--- exercises/scc0210/2022/5.stl-3/main.cpp
+++ exercises/scc0210/2022/5.stl-3/main.cpp
@@ -2,85 +2,78 @@
 
 using namespace std;
 
-bool cmp(pair<int,string> id1,pair<int,string> id2){
-    if(id1.first != id2.first)
-        return id1.first < id2.first;
-    else
-        return id1.second > id2.second;
+// True: id1 < id2
+bool cmp(pair<int, string> id1, pair<int, string> id2) {
+  if (id1.first != id2.first)
+    return id1.first < id2.first;
+  else
+    return id1.second > id2.second;
 }
 
-void make_reading(string input){
-    /* STRUCTURES */
+void print_ans(map<string, set<string>> projects,
+               map<string, string> students) {
+  /* PRINTED */
+  // Control the priority of printed of project
+  // First element: legth
+  // Second element: project name
+  vector<pair<int, string>> printed;
 
-    //Key: student id
-    //Value: project name
-    map<string, string> students;
+  for (auto it : projects) {
+    int cont = 0;
+    for (auto id : it.second)
+      if (students[id] != "-1")
+        cont++;
+    printed.push_back(make_pair(cont, it.first));
+  }
 
-    //List of students erased
-    set<string> erased;
+  // Constructor of the priority_queue
+  /*for (auto it : projects)
+    printed.push_back(make_pair(it.second.size(), it.first));*/
+  sort(printed.rbegin(), printed.rend(), cmp);
 
-    //Projects
-    map<string, set<string>> projects;
-
-    /*ALGORITHM*/
-    string cur_project;
-    while(input != "1"){
-        if('A' <= input[0] && input[0] <= 'Z'){//Input is cur_project
-            cur_project = input;
-            //Create a set to consider empty sets
-            set<string> empty_set;
-            projects[ cur_project ] = empty_set;
-        }else{//Input is student id
-            //If the student isn't in erased set
-            if( erased.find(input) == erased.end() ){
-                //If the students don't have project  OR the project that it have is the same
-                if(students.count(input) == 0 || students[input] == cur_project){
-                    students[input] = cur_project;
-                    projects[cur_project].insert(input);
-                }else{
-                    erased.insert(input);
-                    projects[ students[input] ].erase(input);
-                    students.erase(input);
-                }
-            }
-        }
-        getline(cin, input);
-    }
-
-    /* PRINTED */
-    //Control the priority of printed of project
-    //First element: legth
-    //Second element: project name
-    priority_queue<pair<int,string>, vector<pair<int,string>>, decltype(&cmp) > printed (&cmp);
-
-    //Constructor of the priority_queue
-    for(auto it : projects){
-        printed.push( make_pair(it.second.size(),it.first) );
-    }
-
-    //Printed
-    while(!printed.empty()){
-        cout << printed.top().second << " " << printed.top().first << endl;
-        printed.pop();
-    }
+  // Printed
+  for (auto it : printed)
+    cout << it.second << " " << it.first << endl;
 }
 
-int main(){
-    string input;
+void make_reading(string input) {
+  /* STRUCTURES */
+
+  // Key: student id
+  // Value: project name
+  map<string, string> students;
+
+  // Projects
+  map<string, set<string>> projects;
+
+  /*ALGORITHM*/
+  string cur_project;
+  while (input != "1") {
+    if ('A' <= input[0] && input[0] <= 'Z') { // Input is cur_project
+      cur_project = input;
+      // Create a set to consider empty sets
+      projects[cur_project] = {};
+    } else { // Input is student id
+      if (students.count(input) == 0 || students[input] == cur_project) {
+        students[input] = cur_project;
+        projects[cur_project].insert(input);
+      } else if (students[input] != "-1") {
+        // projects[students[input]].erase(input);
+        students[input] = "-1";
+      }
+    }
     getline(cin, input);
-    while(input != "0"){
-        make_reading(input);
-        getline(cin, input);
-    }
-
-    return 0;
+  }
+  print_ans(projects, students);
 }
 
+int main() {
+  string input;
+  getline(cin, input);
+  while (input != "0") {
+    make_reading(input);
+    getline(cin, input);
+  }
 
-//Name projects
-//Subscribed
-//Again
-
-//Erased: set
-//Dict: id -> project (control the unique)
-//Dict: project -> [ids] (control the students)
+  return 0;
+}
