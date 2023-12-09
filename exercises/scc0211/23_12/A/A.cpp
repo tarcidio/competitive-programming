d3a2cddb9354c86871a7c969cfcc0828e698861b
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2023-12-09 09:46:17 -0300

    Create A.cpp

---
+++ exercises/scc0211/23_12/A/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int num_heaps, size_heap;
+        cin >> num_heaps;
+        int nim_sum = 0;
+        for(int i = 0; i < num_heaps; i++){
+            cin >> size_heap;
+            nim_sum ^= size_heap;
+        }
+        nim_sum ? cout << "first" << endl : cout << "second" << endl;
+    }
+
+    return 0;
+}
