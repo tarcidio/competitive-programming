62b0995db9ad3c9229ba08e4b09b1170b66bd9fd
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-18 09:15:53 -0300

    Update main.cpp

--- exercises/neetcode/two_pointer/125. Valid Palindrome/main.cpp
+++ exercises/neetcode/two_pointer/125. Valid Palindrome/main.cpp
@@ -1,1 +1,30 @@
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
 
+        // Compare left and right in the same time
+        while(left < right){
+            // Find the next valid left character
+            while(!isalnum(s[left]) && left < right)
+                left++;
+
+            // Find the next valid right character
+            while(!isalnum(s[right]) && left < right)
+                right--;
+
+            // Check
+            if(tolower(s[left]) != tolower(s[right]))
+                return false;
+            
+            // Increment
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
