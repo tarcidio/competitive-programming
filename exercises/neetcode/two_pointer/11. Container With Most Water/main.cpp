14525c8276ef1acadb567ab1147b7f9388ab1d1f
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-19 09:00:31 -0300

    Update main.cpp

--- exercises/neetcode/two_pointer/11. Container With Most Water/main.cpp
+++ exercises/neetcode/two_pointer/11. Container With Most Water/main.cpp
@@ -1,1 +1,23 @@
+//Time: O(N)
+//Space: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int ans = 0;
 
+        while(l < r){
+            ans = max(ans, (r - l)*min(height[l], height[r]) );
+            //The only chance to increase the area is 
+            //if the next height is greater than the previous one
+            //as we will decrease the length of the area
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return ans;
+    }
+};
