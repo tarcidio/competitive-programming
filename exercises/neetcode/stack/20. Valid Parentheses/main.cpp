b3b9748dee8456931b4ad96b03a2ef490a7a7928
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-19 09:54:12 -0300

    Update main.cpp

--- exercises/neetcode/stack/20. Valid Parentheses/main.cpp
+++ exercises/neetcode/stack/20. Valid Parentheses/main.cpp
@@ -1,1 +1,19 @@
+//Time: O(N)
+// Space: O(N)
+class Solution {
+public:
+    bool isValid(string s) {
+        if(!s.size()%2)
+            return false;
+        
+        stack<char> brackets;
+        for(auto bracket: s)
+            if(bracket == '{') brackets.push('}');
+            else if(bracket == '[') brackets.push(']');
+            else if(bracket == '(') brackets.push(')');
+            else if (brackets.top() == bracket) brackets.pop();
+            else return false;
 
+        return true;
+    }
+};
