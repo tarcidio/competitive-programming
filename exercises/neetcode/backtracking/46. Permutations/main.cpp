57bc812937ac847cb50c8266be7cdcde1dd2df5a
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-27 16:32:01 -0300

    Create main.cpp

---
+++ exercises/neetcode/backtracking/46. Permutations/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void makePermutation(int start, vector<int>& nums, vector<vector<int>>& ans){
+        if(start >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = start; i < nums.size(); i++){
+            // Fix one
+            swap(nums[start], nums[i]);
+            // Calculate permutations permuting remaining elements
+            makePermutation(start + 1, nums, ans);
+            // Reverte to start new loop
+            swap(nums[start], nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+        makePermutation(0, nums, ans);
+
+        return ans;        
+    }
+};
