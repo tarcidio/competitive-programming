142ccbafaffd9f69a44fe078f0c1f91ba69d7911
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-27 16:04:03 -0300

    Create main.cpp

---
+++ exercises/neetcode/backtracking/39. Combination Sum/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void dfs(int i, int target, int curSum, vector<int> &candidates, vector<int> &temp, vector<vector<int>> &ans){
+        if(i >= candidates.size() || curSum > target)
+            return;
+        else if(curSum == target){
+            ans.push_back(temp);
+            return;
+        }
+        // If consider this value, consider a repetition
+        temp.push_back(candidates[i]);
+        dfs(i, target, curSum + candidates[i], candidates, temp, ans);
+        // If don't, consider the next value
+        temp.pop_back(); 
+        dfs(i + 1, target, curSum, candidates, temp, ans);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+
+        dfs(0, target, 0, candidates, temp, ans);
+
+        return ans;        
+    }
+};
